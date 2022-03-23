c7c4972cb6d272e26453ae51d10c8a93df331909
aidanflinton <69019017+aidanflinton@users.noreply.github.com>
2022-03-23 12:43:07 -0400

    onClick 'click'

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -3,20 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = { 
-      value: null,
-    }
-  }
-
   render() {
     return (
-      <button 
-        className="square" 
-        onClick={() => this.setState({value: 'X'})}
-      >
-        {this.state.value}
+      <button className="square" onClick = {() => console.log("click")}>
+        {this.props.value}
       </button>
     );
   }
@@ -24,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value = {i}/>;
+    return <Square value = {i} />;
   }
 
   render() {
